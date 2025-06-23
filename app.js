3560d1b6e06f9d1977c7c7df9db4b85d214ef4ef
Prasshanthini R <107397262+Prassha0908@users.noreply.github.com>
2025-06-23 10:49:24 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const admin = require('firebase-admin');
 const cors = require('cors');
-const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY);
 
 const app = express();
 const PORT = process.env.PORT || 5000;
